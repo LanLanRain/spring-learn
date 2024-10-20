bddd27e70c76a2be89a4ce44d79d2fd80ceed164
LunerBreeze <RainSoul1120@gmail.com>
2024-10-20 16:57:12 +0800

    ApplicationContext常用API

--- src/test/java/com/lb/TestSpring.java
+++ src/test/java/com/lb/TestSpring.java
@@ -2,6 +2,8 @@ package com.lb;
 
 import com.lb.basic.*;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
 
@@ -29,4 +31,55 @@ public class TestSpring {
         Person person = (Person) BeanFactory.getBean("person");
         System.out.println("person = " + person);
     }
+
+    @Test
+    public void test4() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = applicationContext.getBean("person", Person.class);
+        System.out.println("person = " + person);
+    }
+
+    @Test
+    public void test5() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = applicationContext.getBean(Person.class);
+        System.out.println("person = " + person);
+    }
+
+    @Test
+    public void test6() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+
+        /*
+         * beanDefinitionName = person
+         * beanDefinitionName = person1
+         */
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("beanDefinitionName = " + beanDefinitionName);
+        }
+
+        String[] beanNamesForType = applicationContext.getBeanNamesForType(Person.class);
+
+        /*
+         * beanName = person
+         * beanName = person1
+         */
+        for (String beanName : beanNamesForType) {
+            System.out.println("beanName = " + beanName);
+        }
+
+        boolean person = applicationContext.containsBean("person");
+        boolean person1 = applicationContext.containsBean("person1");
+
+        System.out.println("person = " + person);
+        System.out.println("person1 = " + person1);
+
+        applicationContext.containsBeanDefinition("person");
+        applicationContext.containsBeanDefinition("person1");
+    }
+
+
+
+
 }
