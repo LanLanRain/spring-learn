86e26da2c11b72dffae7c48f3af8ee5b67adf3c9
LunerBreeze <RainSoul1120@gmail.com>
2024-10-20 23:17:27 +0800

    进一步体会依赖注入，connection四个字符串参数

--- src/main/java/com/lb/factorybean/ConnectionFactoryBean.java
+++ src/main/java/com/lb/factorybean/ConnectionFactoryBean.java
@@ -6,10 +6,47 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionFactoryBean implements FactoryBean<Connection> {
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public Connection getObject() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis_learn", "root", "mysql_1120");
+        Class.forName(driverClassName);
+        return DriverManager.getConnection(url, username, password);
     }
 
     @Override
